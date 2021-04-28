351b157751e7a045415a628a4a8c6821c7f5a8ac
Erick2418 <erickjoellimon@outlook.com>
2021-04-27 22:54:12 -0500

    Optimizando el component y usando el hook

--- src/components/GifGrif.js
+++ src/components/GifGrif.js
@@ -1,34 +1,28 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-//import { getGifs } from '../helpers/getGifs';
-//import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrif = ({category}) => {
 
-    // const [images, setimages] = useState([]);
-    const {data, loading} = useFetchGifs();
-    console.log(data,loading);
-    // useEffect( ()=>{
-    //     getGifs(category)
-    //     .then(setimages)// Forma extensa:imgs=>{ setimages(imgs)}
-    // },[category]);
-
-
-
+    const {data:images, loading} = useFetchGifs(category);
+   /*
+   DE ESTA MANERA YA QUEDA EL COMPONENTE MUY LIMPIO
+   TODO LO PESADO QUEDA EN EL CUSTOM HOOK
+   */
     return (    
         <>
         <h3>{category}</h3>
-        {loading? 'Cargando...': 'Data cargada con exito!'}
-        {/* <div className="card-grid">       
+        {loading && <p>Loading</p>}
+        <div className="card-grid">       
                 {
                     images.map( img=>(
                       <GifGridItem
                         key={img.id}  
-                        {...img }// se envian de manera independiente
+                        {...img }
                       />
                      ))
                 } 
-        </div> */}
+        </div>
         </>
     )
 }
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,17 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import {getGifs} from './../helpers/getGifs'
 
+export const useFetchGifs=(category)=>{
 
-
-export const useFetchGifs=()=>{
     const [state, setstate] = useState({
         data:[],
         loading:true
     });
-    setTimeout( () => {
-        setstate({
-            data:[1,2,3,4,5,6,7],
-            loading:false
-        })
-    }, 3000);
-    return state; //{data: [] , loading: true}
+
+
+    useEffect(()=>{
+
+        getGifs(category)
+            .then(imgs=>{
+
+                setTimeout(() => {
+
+                    setstate({    
+                        data:imgs,
+                        loading:false
+                    })
+
+                }, 3000);
+                
+            })
+
+    },[category]);
+ 
+    return state; 
 }
